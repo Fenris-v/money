eedefcb38e3daf0831b9cd79eba561e3bfab8897
Fenris <anatolijv236@gmail.com>
2020-06-18 20:30:44 +0300

    add modal and scripts for calculator

--- js/scripts.js
+++ js/scripts.js
@@ -21,49 +21,97 @@ $(document).ready(() => {
     });
 
     /**
-     * SLIDERS
+     * SLIDERS (CALCULATOR)
      */
-    $('#rangeSlider_first').slider({
+    let firstSlider = $('#rangeSlider_first');
+    let creditSum = $('#creditSum');
+    firstSlider.slider({
         range: "min",
         value: 2000,
         min: 2000,
         max: 2000000,
         slide: function (event, ui) {
-            $("#creditSum").val(ui.value.toLocaleString(undefined, 'ru-RU'));
+            creditSum.val(ui.value.toLocaleString(undefined, 'ru-RU'));
+            calculate();
         },
     });
     $("#creditSum").val(($("#rangeSlider_first").slider("value")).toLocaleString(undefined, 'ru-RU'));
 
-    $('#rangeSlider_second').slider({
+    let secondSlider = $('#rangeSlider_second');
+    let creditTerm = $('#creditTerm');
+    secondSlider.slider({
         range: "min",
         value: 1,
         min: 1,
         max: 60,
         slide: function (event, ui) {
-            $("#creditTerm").val(ui.value);
+            creditTerm.val(ui.value);
+            calculate();
         },
     });
-    $("#creditTerm").val($("#rangeSlider_second").slider("value"));
-
-    $('#rangeSlider_third').slider({
-        range: "min",
-        value: 2000,
-        min: 2000,
-        max: 2000000,
-        slide: function (event, ui) {
-            $("#monthPay").val(ui.value.toLocaleString(undefined, 'ru-RU'));
-        },
-    });
-    $("#monthPay").val(($("#rangeSlider_third").slider("value")).toLocaleString(undefined, 'ru-RU'));
+    creditTerm.val($("#rangeSlider_second").slider("value"));
 
     $('.rangeSlider .ui-slider-handle').on('mousedown', (e) => {
         $(e.target).css('background', '#d51c2a');
         $(e.target).siblings('.rangeSlider .ui-widget-header').css('background', '#d51c2a');
     });
 
-    $(document).on('mouseup', (e) => {
+    $(document).on('mouseup', () => {
         $('.rangeSlider .ui-slider-handle').css('background', '#82766d');
         $('.rangeSlider .ui-widget-header').css('background', '#82766d');
+    });
+
+    creditSum.on('blur', () => {
+        firstSlider.slider("value", parseInt(creditSum.val().replace(/\s/g, '')));
+        calculate();
+    });
+
+    creditTerm.on('blur', () => {
+        secondSlider.slider("value", parseInt(creditTerm.val().replace(/\s/g, '')));
+        calculate();
+    });
+
+    /**
+     * CALCULATE
+     */
+    let percentForPay = $('#percentForPay');
+    let carChecked = $('.whereCar:checked');
+    percentForPay.text(carChecked.data('percent'));
+    percentForPay.attr('data-percent', carChecked.data('percent'));
+
+    $('.whereCar').on('change', () => {
+        let carChecked = $('.whereCar:checked');
+        percentForPay.text(carChecked.data('percent'));
+        percentForPay.attr('data-percent', carChecked.data('percent'));
+        calculate();
+    });
+
+    let calculate = () => {
+        let monthPay = $('#monthPay');
+        let sumForPay = $('#sumForPay');
+
+        let percent = parseFloat(percentForPay.attr('data-percent')) / 100 + 1;
+        let sum = parseInt(creditSum.val().replace(/\s/g, ''));
+        let month = parseInt(creditTerm.val());
+
+        let allSum = sum * percent;
+        let monthSum = allSum/month;
+
+        sumForPay.text(parseInt(allSum).toLocaleString(undefined, 'ru-RU'));
+        sumForPay.attr('data-full-sum', parseInt(allSum));
+
+        monthPay.text(parseInt(monthSum).toLocaleString(undefined, 'ru-RU'));
+        monthPay.attr('data-month-pay', parseInt(monthSum));
+    }
+
+    calculate();
+
+    let submitCredit = $('#submitCredit');
+    $('#calculatorSubmit').on('click', () => {
+        submitCredit.attr('data-percent', $('#percentForPay').attr('data-percent'));
+        submitCredit.attr('data-full-sum', $('#sumForPay').attr('data-full-sum'));
+        submitCredit.attr('data-month', $('#creditTerm').val());
+        submitCredit.attr('data-month-pay', $('#monthPay').attr('data-month-pay'));
     });
 
     /**
@@ -86,6 +134,26 @@ $(document).ready(() => {
     radio.on('change', () => {
         customRadioAll.css('background', '#ffffff')
         checkCustom();
+    });
+
+    /**
+     * CHEKBOX
+     */
+    let checkRules = $('#checkRules');
+    let customCheck = $('.customCheck');
+    checkRules.css('opacity', 0);
+    customCheck.css('display', 'block');
+
+    if (checkRules.prop('checked')) {
+        customCheck.css('background', '#d51c2a');
+    }
+
+    checkRules.on('change', () => {
+        if (checkRules.prop('checked')) {
+            customCheck.css('background', '#d51c2a');
+        } else {
+            customCheck.css('background', '#ffffff');
+        }
     });
 
     /**
@@ -119,4 +187,20 @@ $(document).ready(() => {
      * MASK
      */
     $('#phone').mask("+7 (999) 999-99-99");
+    $('#modalPhone').mask("+7 (999) 999-99-99");
+
+    /**
+     * MODAL
+     */
+    $('[data-fancybox-modal]').fancybox({
+        trapFocus: true,
+        autoFocus: false,
+        touch: false,
+        beforeShow: function() {
+            $('html').addClass('scroll-disable');
+        },
+        afterClose: function() {
+            $('html').removeClass('scroll-disable');
+        }
+    });
 });
